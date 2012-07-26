0a46b3fb208b016acdfa1d28a24f10cfd398ae43
wendiewang <wendiewang@gmail.com>
2012-07-26 16:53:45 -0700

    linked to admin database

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -3,11 +3,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import loader, RequestContext
+from django.views.decorators.http import require_http_methods
+from models import Post
+
+#@require_http_methods(["POST"])
+# def home(request):
+# 	content = "hello"
+# 	return render_to_response('wendy_template.html',{ 'content': content })
 
 def home(request):
+	post = Post.objects.get(id=2)
 	return render_to_response('wendy_template.html',
-                         { },
+                         dict(post=post), 
                          context_instance=RequestContext(request))
-	#return HttpResponse("Wendy Wang website coming soon!")
-
-
