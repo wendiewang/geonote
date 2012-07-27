3b8853db7c05b3df2e5c8f4c4723b84d6c82608f
wendiewang <wendiewang@gmail.com>
2012-07-26 23:52:13 -0700

    working admin

--- webapp/main/views.py
+++ webapp/main/views.py
@@ -4,7 +4,9 @@ from django.http import HttpResponse
 from django.shortcuts import render_to_response
 from django.template import loader, RequestContext
 from django.views.decorators.http import require_http_methods
+from django.core import serializers
 from models import Post
+
 
 #@require_http_methods(["POST"])
 # def home(request):
@@ -12,7 +14,8 @@ from models import Post
 # 	return render_to_response('wendy_template.html',{ 'content': content })
 
 def home(request):
-	post = Post.objects.get(id=2)
+	posts = Post.objects.all()
+	postsjson = serializers.serialize('json', posts)
 	return render_to_response('wendy_template.html',
-                         dict(post=post), 
+                         dict(posts=postsjson),
                          context_instance=RequestContext(request))
